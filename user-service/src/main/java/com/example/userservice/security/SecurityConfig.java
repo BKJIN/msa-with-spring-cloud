63ef9c42457a46a3ab5ce210d1316e580c50e3cf
Jimoo An <jmdh1004@gmail.com>
2023-04-17 22:39:32 +0900

    Users Microservice

---
+++ user-service/src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.example.userservice.security;
+
+import com.example.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private AuthenticationConfiguration authenticationConfiguration;
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    @Autowired
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, Environment env,
+                          UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.1.79")
+                        .and()
+                                .addFilter(getAuthenticationFilter());
+        http.headers().frameOptions().disable(); //frame으로 나뉘어져 있는 html 페이지 보이게 함(h2-console 페이지)
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception{
+        return this.authenticationConfiguration.getAuthenticationManager();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception{
+        AuthenticationFilter authenticationFilter =
+                new AuthenticationFilter(authenticationManager(), userService, env);
+//        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    // 인증
+    // select pwd from users where email = ?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        return auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder).and().build();
+    }
+
+}
